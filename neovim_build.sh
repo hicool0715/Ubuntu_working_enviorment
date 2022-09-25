8a030ed0dae67727ff23087fae962d67782fbe16
hicool0715 <18870405316@163.com>
2022-09-25 23:04:44 +0800

    add neovim build init files, not finished, neovim scripts left

--- neovim_build.sh
+++ neovim_build.sh
@@ -26,4 +26,18 @@ fi
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install  -j`cat /proc/cpuinfo| grep "processor"| wc -l`
 
+echo "Build neovim plugs(Y/N)"
+read anwers
+if [ "$anwers" != "Y" ]; then 
+	exit 1
+fi
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+if [ ! -d /home/$USER/.config/nvim ]; then
+	mkdir -p /home/$USER/.config/nvim
+fi
+
+
 
